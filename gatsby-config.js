30570534c70519494c90946051585c52ef6c3211
0xhjohnson <0xhjohnson@protonmail.com>
2020-02-05 11:53:02 -0500

    Updated gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Pop'z BBQ & Catering - Bad to the Bone`,
+    description: `Pop’z BBQ & Catering was developed with one thing in mind; to provide people with delicious, cooked to order South Georgia style BBQ. Developing a reputation for some of the best South Georgia style BBQ. Our menu has expanded over the years and includes a large variety of options that are truly delicious.`,
+    author: `@0xhjohnson`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Pop'z BBQ`,
+        short_name: `Pop'z BBQ`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
