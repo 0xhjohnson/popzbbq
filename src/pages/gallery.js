f721ac3c28b2d802aeb46702bb1306ecfc29d4ce
0xhjohnson <0xhjohnson@protonmail.com>
2020-02-12 16:57:08 -0500

    Implemented gallery page and added images

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import { Heading } from 'theme-ui';
+import Img from 'gatsby-image';
+import { Heading, Grid, Box } from 'theme-ui';
+import { graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const Gallery = () => (
-  <Layout>
-    <SEO title="Gallery" />
-    <Heading as="h1">Gallery</Heading>
-  </Layout>
-);
+const Gallery = () => {
+  const { allFile } = useStaticQuery(graphql`
+    query {
+      allFile(
+        filter: {
+          extension: { regex: "/(jpg)|(jpeg)|(png)/" }
+          sourceInstanceName: { eq: "images" }
+          relativeDirectory: { eq: "gallery" }
+        }
+      ) {
+        edges {
+          node {
+            childImageSharp {
+              fluid(quality: 80) {
+                originalName
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Layout>
+      <SEO title="Gallery" />
+      <Heading as="h1" my={2}>
+        Gallery
+      </Heading>
+      <Grid gap={3} columns={[null, 2, 3]} py={3}>
+        {allFile.edges.map(({ node }) => (
+          <Img key={node.id} fluid={node.childImageSharp.fluid} />
+        ))}
+      </Grid>
+    </Layout>
+  );
+};
 
 export default Gallery;
