840a6175689623dc9f0586c73976db237f584651
0xhjohnson <0xhjohnson@protonmail.com>
2020-03-28 17:06:26 -0400

    Styling fixes to the about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,19 +6,19 @@ import SEO from '../components/SEO';
 const About = () => (
   <Layout title="About">
     <SEO title="About" />
-    <div className="mx-auto max-w-xl">
-      <h3 className="my-3">
+    <div className="mt-8 mb-12 mx-auto max-w-3xl">
+      <h3 className="mb-6 text-xl font-medium text-gray-700">
         We provide South Georgia style BBQ that people love, day after day after
         day. It is not a big secret. People just want more of it.
       </h3>
-      <p className="my-3">
+      <p className="mb-3 text-gray-900">
         Pop&apos;z BBQ &amp; Catering was developed with one thing in mind; to
         provide people with delicious, cooked to order South Georgia style BBQ.
         Creating a reputation for some of the best South Georgia style BBQ. Our
         menu has expanded over the years and now includes a large variety of
         options ranging from slow-cooked pulled pork to custom cut steaks.
       </p>
-      <p className="mt-2 mb-3">
+      <p className="text-gray-900">
         Pop&apos;z BBQ can take care of all types of events including weddings.
         Whether your event is for 50 people or 1,500 I always provide the
         highest service possible and ensure everything goes extremely smooth. I
