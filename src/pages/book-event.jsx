63afd4ae8ea3ff267899877eb7b83ccab9b95bf7
0xhjohnson <0xhjohnson@protonmail.com>
2020-03-28 18:12:41 -0400

    Fixed up issues on the book-event page

--- src/pages/book-event.jsx
+++ src/pages/book-event.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import { useForm, ErrorMessage } from 'react-hook-form';
 import { object, string, date } from 'yup';
+import classNames from 'classnames';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -41,113 +42,136 @@ const BookEvent = () => {
 
   const handleChange = (d) => setValue('eventDate', d, true);
 
+  const inputClasses = [
+    'form-input',
+    'block',
+    'w-full',
+    'text-gray-900',
+    'sm:text-sm',
+    'sm:leading-5'
+  ];
+
   return (
     <Layout title="Book an Event">
       <SEO title="Book an Event" />
-      <div className="mx-0 lg:mx-4 xl:mx-5 mt-0 lg:mt-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-1 md:gap-4">
-          <h4 className="my-3">
+      <div className="mt-8 mb-12 mx-auto max-w-5xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <h3 className="text-gray-900">
             I look forward to catering your next event and am here to answer any
             questions you may have. Fill out the contact form below with details
             regarding your event and I will personally get back with you within
             24 hours.
-          </h4>
+          </h3>
           <form onSubmit={handleSubmit(onSubmit)}>
             <label
               htmlFor="name"
-              className="block text-sm leading-5 font-medium text-gray-700 mt-0 md:mt-2"
+              className="block text-sm leading-5 font-medium text-gray-700"
             >
               Name
               <input
                 id="name"
                 name="name"
-                className="form-input block w-full sm:text-sm sm:leading-5"
+                className={classNames(inputClasses, {
+                  'border-red-700': errors.name
+                })}
                 ref={register}
               />
             </label>
             <ErrorMessage errors={errors} name="name">
               {({ message }) => (
-                <p className="color-red-700 text-sm">{message}</p>
+                <p className="text-red-700 text-sm">{message}</p>
               )}
             </ErrorMessage>
             <label
               htmlFor="email"
-              className="block text-sm leading-5 font-medium text-gray-700 mt-0 md:mt-3"
+              className="block text-sm leading-5 font-medium text-gray-700 mt-4"
             >
               Email
               <input
                 id="email"
                 name="email"
-                className="form-input block w-full sm:text-sm sm:leading-5 mb-2"
+                className={classNames(inputClasses, {
+                  'border-red-700': errors.email
+                })}
                 ref={register}
               />
             </label>
             <ErrorMessage errors={errors} name="email">
               {({ message }) => (
-                <p className="color-red-700 text-sm">{message}</p>
+                <p className="text-red-700 text-sm">{message}</p>
               )}
             </ErrorMessage>
             <label
               htmlFor="phone"
-              className="block text-sm leading-5 font-medium text-gray-700 mt-0 md:mt-3"
+              className="block text-sm leading-5 font-medium text-gray-700 mt-4"
             >
               Phone
               <input
                 id="phone"
                 name="phone"
-                className="form-input block w-full sm:text-sm sm:leading-5 mb-2"
+                className={classNames(inputClasses, {
+                  'border-red-700': errors.phone
+                })}
                 ref={register}
               />
             </label>
             <ErrorMessage errors={errors} name="phone">
               {({ message }) => (
-                <p className="color-red-700 text-sm">{message}</p>
+                <p className="text-red-700 text-sm">{message}</p>
               )}
             </ErrorMessage>
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label
               htmlFor="eventDate"
-              className="block text-sm leading-5 font-medium text-gray-700 mt-0 md:mt-3"
+              className="block text-sm leading-5 font-medium text-gray-700 mt-4"
             >
               Event date
-              <DatePicker
-                onChange={handleChange}
-                selected={eventDate}
-                showTimeSelect
-                timeIntervals={30}
-                timeCaption="Time"
-                dateFormat="MMM d yyyy h:mm aa"
-                customInput={
-                  <input id="eventDate" name="eventDate" className="m-2" />
-                }
-              />
             </label>
+            <DatePicker
+              onChange={handleChange}
+              selected={eventDate}
+              showTimeSelect
+              timeIntervals={30}
+              timeCaption="Time"
+              dateFormat="MMM d yyyy h:mm aa"
+              customInput={
+                <input
+                  id="eventDate"
+                  name="eventDate"
+                  className={classNames(inputClasses, {
+                    'border-red-700': errors.eventDate
+                  })}
+                />
+              }
+            />
             <ErrorMessage errors={errors} name="eventDate">
               {() => (
-                <p className="color-red-700 text-sm">
+                <p className="text-red-700 text-sm">
                   Event date is a required field.
                 </p>
               )}
             </ErrorMessage>
             <label
               htmlFor="details"
-              className="block text-sm leading-5 font-medium text-gray-700 mt-0 md:mt-3"
+              className="block text-sm leading-5 font-medium text-gray-700 mt-4"
             >
               Details
               <textarea
                 id="details"
                 name="details"
-                className="form-input block w-full sm:text-sm sm:leading-5 mb-2"
+                className={classNames(inputClasses, {
+                  'border-red-700': errors.details
+                })}
                 rows="5"
                 ref={register}
               />
             </label>
             <ErrorMessage errors={errors} name="details">
               {({ message }) => (
-                <p className="color-red-700 text-sm">{message}</p>
+                <p className="text-red-700 text-sm">{message}</p>
               )}
             </ErrorMessage>
-            <button type="button" className="btn btn-primary">
+            <button type="submit" className="btn btn-orange mt-6">
               Submit
             </button>
           </form>
